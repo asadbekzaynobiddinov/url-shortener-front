f747ac689e15dcc1e256827d7c9078009d8617cf
asadbekzaynobiddinov <asadbekzaynobiddinov51@gmail.com>
2025-02-22 19:20:38 +0500

    Update

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://100.26.23.20:3030/url',
+  baseURL: 'https://100.26.23.20:3030',
 });
 
 export default api;
--- src/api/urlService.ts
+++ src/api/urlService.ts
@@ -1,17 +1,17 @@
 import api from './api';
 
 export const createShortUrl = async (originalUrl: string, alias?: string) => {
-  const response = await api.post('/shorten', { originalUrl, alias });
+  const response = await api.post('/url/shorten', { originalUrl, alias });
   console.log(response);
   return response.data;
 };
 
 export const getUrlInfo = async (shortUrl: string) => {
-  const response = await api.get(`/info/${shortUrl}`);
+  const response = await api.get(`/url/info/${shortUrl}`);
   return response.data;
 };
 
 export const deleteShortUrl = async (shortUrl: string) => {
-  const response = await api.delete(`/delete/${shortUrl}`);
+  const response = await api.delete(`/url/delete/${shortUrl}`);
   return response.data;
 };
